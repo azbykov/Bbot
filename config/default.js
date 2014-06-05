0af7b0b38d8d6d5902cd271e99d34fcd42a06b72
Aleksandr Bykov <azbykov@yamoney.ru>
2014-06-05 11:44:25 +0400

    v0.2.2 update modules

--- config/default.js
+++ config/default.js
@@ -7,15 +7,20 @@ module.exports = {
 		},
 
 		auth: {
-			login: 'mygen',
-			password: 2770236
+			login: 'login',
+			password: 'password'
 		},
 
 		path: {
-			"domain": 'http://www.butsa.ru',
-			"auth": '/index.php?login=1',
-			"getOrder": '/builder',
-			"sendOrder": '/builder/test.php?start_debug=1'
+			protocol: 'http://',
+			domain: 'www.butsa.ru',
+			auth: '/index.php?login=1',
+			office: '/office',
+			order: '/builder',
+			orderParams: '/test.php?start_debug=1',
+			friendly: '/xml/office/organizer.php?type=office/organizer&act=friendly',
+			goods: '/xml/finance/shop.php?type=finance/shop&act=buy',
+			buildings: '/xml/team/buildings.php?type=team/buildings&act=repairall2'
 		},
 
 		team: [
@@ -27,21 +32,46 @@ module.exports = {
 				"name": "international",
 				"id": 10687
 			}
-		]
+		],
+
+//		orderName: 'test'
+		orderName: 'auto',
+		friendly: {
+		},
+		goods: {
+			goods: 300000,
+			games: 2
+		},
+		buildings: {
+		}
 	},
 
-    log: {
-        transports: {
-            console: {
-                level: 'debug',
-                colorize: true
-            },
-            file: {
-                level: 'debug',
-                filename: path.join(__dirname, '../log/', 'node.log'),
-                json: false,
-                timestamp: true
-            }
-        }
-    }
+	log: {
+		transports: {
+			console: {
+				level: 'debug',
+				colorize: true
+			},
+			file: {
+				level: 'debug',
+				filename: path.join(__dirname, '../log/', 'node.log'),
+				json: false,
+				timestamp: function() {
+					var date = new Date();
+					var dateArr = [
+						date.getDate(),
+						date.getMonth()+1,
+						date.getFullYear()
+					];
+					var timeArr =[
+						('00' + date.getHours()).slice(-2),
+						('00' + date.getMinutes()).slice(-2),
+						('00' + date.getSeconds()).slice(-2),
+						('000' + date.getMilliseconds()).slice(-3)
+					];
+					return dateArr.join('.') + ' ' + timeArr.join(':')
+				}
+			}
+		}
+	}
 };
